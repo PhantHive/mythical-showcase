d3dbcc9bcba4c4a581381cbef8096b95d0f0d4d0
PhantHive <phantomhive72@yahoo.fr>
2024-12-29 20:25:30 +0100

    chore: trying without the cookie notif

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 'use client';
 
-import CookieConsent from '@/components/cookies/CookieConsent';
+// import CookieConsent from '@/components/cookies/CookieConsent';
 import AppRouter from '@/components/router/AppRouter';
 
 export default function RootPage() {
     return (
         <>
             <AppRouter />
-            <CookieConsent />
+            {/*<CookieConsent />*/}
         </>
     );
 }
